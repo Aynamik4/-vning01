5ddcbd5e0c64361bafb977615efd887f9d319a58
Aynamik4 <violintailgut@gmail.com>
2025-04-01 14:45:08 +0200

    Varsion 1

---
+++ Övning01/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning01;
+internal class Employee
+{
+    public Employee(string name, decimal salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Salary: {Salary}";
+    }
+}
--- Övning01/Program.cs
+++ Övning01/Program.cs
@@ -1,10 +1,99 @@
-﻿namespace Övning01
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Övning01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool stopApp = false;
+            List<Employee> employees = new List<Employee>();
+
+            while (!stopApp)
+            {
+                switch (PrintMenu())
+                {
+                    case ConsoleKey.A:
+                        AddEmployee(employees);
+                        break;
+                    case ConsoleKey.L:
+                        ListEmployees(employees);
+                        break;
+                    case ConsoleKey.S:
+                        stopApp = true;
+                        break;
+                }
+            }
+        }
+
+        private static void ListEmployees(List<Employee> employees)
+        {
+            ClearConsole();
+            Console.WriteLine("-----------------------------------------");
+            foreach (Employee employee in employees)
+            {
+                WriteLine($"Name:\t{employee.Name}");
+                WriteLine($"Salary:\t{employee.Salary}");
+                Console.WriteLine("-----------------------------------------");
+            }
+
+            WriteLine();
+            Write("Press any key to continue...");
+            ReadKey(true);
+        }
+
+        private static void AddEmployee(List<Employee> employees)
+        {
+            ClearConsole();
+
+            string name = string.Empty;
+            do
+            {
+                Write("Enter employee NAME: ");
+                name = ReadLine();
+            } while (name.Length < 2);
+
+            ClearConsole();
+
+            string salaryStr = string.Empty;
+            bool convertOK;
+            decimal salary;
+
+            do
+            {
+                Write("Enter employee SALARY: ");
+                salaryStr = ReadLine();
+                convertOK = decimal.TryParse(salaryStr, out salary);
+            } while (!convertOK);
+
+            employees.Add(new Employee(name, salary));
+        }
+
+        static void ClearConsole()
+        {
+            Clear();
+            WriteLine("--- Personalregister ---\n\r");
+        }
+
+        private static ConsoleKey PrintMenu()
+        {
+            ConsoleKey key;
+
+            do
+            {
+                Clear();
+                WriteLine("--- Personalregister ---\n\r");
+                WriteLine("[A]ddera ny anställd");
+                WriteLine("[L]ista anställda");
+                WriteLine("[S]toppa appen!");
+                Write("=> ");
+
+                key = ReadKey().Key;
+            } while (!(key == ConsoleKey.A || key == ConsoleKey.L || key == ConsoleKey.S));
+
+            return key;
         }
     }
 }
